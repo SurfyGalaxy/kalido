2f392cacfd3b56e6ec91c96f4da9b48d21c59155
SurfyGalaxy <ashton3141@gmail.com>
2026-06-13 16:55:22 +1000

    Added presets

--- Main.py
+++ Main.py
@@ -9,19 +9,29 @@
 """
 
 import sys
+import os
+import pathlib
+import yaml
 import functions as func
 import argparse
+import random
+
+script_dir = pathlib.Path(__file__).parent
+presets_path = script_dir / "presets.yaml"
+with open(presets_path) as f: # Copied from my other project, Clide
+    presets = yaml.safe_load(f)
 
 STATE = "NORMAL"
 seq = ""
 rgb_stops = []
 
+sys.stdout.reconfigure(line_buffering=True)
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
     "--stops",
     nargs="+",
-    default=["#E40303", "#FF8C00", "#FFED00", "#008026", "#004CFF", "#732982"],
+    default=["random"],
     help="gradient color stops (hex values)",
 )
 parser.add_argument(
@@ -30,12 +40,29 @@ parser.add_argument(
     default=20,
     help="The amount of intermediate values to add between stops",
 )
+parser.add_argument(
+    "--size",
+    type=int,
+    default=5,
+    help="How large each colour is before moving to next colour"
+)
 args = parser.parse_args()
 stops = args.stops
 count = args.count
+size = args.size
 # This is a list of hex codes with the # still
-stops = func.create_gradient(stops, count)
+
+if stops[0] in presets:
+    stops = presets[stops[0]]
+elif stops[0] == "random":
+    random_flag = random.choice(list(presets.keys()))
+    stops = presets[random_flag]
+
+    
+
+stops = func.create_gradient(stops, count + 1)
 index = 0
+size_index = size
 
 while True:
     c = sys.stdin.read(1)
@@ -48,10 +75,15 @@ while True:
             seq = c
         else:
             sys.stdout.write(func.colourise(stops, c, index))
-            if index == len(stops) -1:
-                index = 0
+            sys.stdout.flush()
+            if size_index != 1:
+                size_index -= 1
             else:
-                index += 1
+                if index == len(stops) -1:
+                    index = 0
+                else:
+                    index += 1
+                size_index = size
 
     elif STATE == "ESC":
         seq += c
@@ -61,6 +93,7 @@ while True:
         else:
             # not CSI, just output and reset
             sys.stdout.write(seq)
+            sys.stdout.flush()
             STATE = "NORMAL"
 
     elif STATE == "CSI":
@@ -70,7 +103,11 @@ while True:
 
             if c != "m":
                 sys.stdout.write(seq)  # keep non-colour sequences
+                sys.stdout.flush()
 
             # always reset once
             STATE = "NORMAL"
             seq = ""
+
+sys.stdout.write('\n')
+sys.stdout.flush()
